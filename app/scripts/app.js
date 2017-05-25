26b4f74460796f959698ecac9ba414f499f84d5e
Rebecca Bennitt <rebecca.m.bennitt@gmail.com>
2017-05-25 14:53:11 -0500

    assignment 3

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+(function() {
+     function config($stateProvider, $locationProvider) {
+
+	     $locationProvider
+	     	.html5mode({
+		     	enabled: true,
+		     	requiredBase: false
+	     	});
+
+	     	$stateProvider
+	     		.state('landing', {
+		     		url: '/',
+		     		templateUrl: '/templates/landing.html'
+	     		})
+	     		.state('album', {
+		     		url: '/album',
+		     		templateUrl: '/templates/album.html'
+	     		})
+	     		.state('collection', {
+		     		url:'/collection',
+		     		templateUrl: '/templates/collection.html'
+	     		});
+     }
+
+     angular
+         .module('audie', ['ui.router'])
+         .config(config);
+ })();
+
+;
